04c4b5b6a14de402110aceee32077a881f431431
Davi-Silva <davi.cunha.silva.ca@gmail.com>
2020-07-01 13:48:20 -0300

    first commit.

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState, useRef, Fragment } from 'react'
 
-import { ExampleComponent } from 'react-image-uploader'
+import { ImageUploader } from 'react-image-uploader'
 import 'react-image-uploader/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [imagesArray, setImagesArray] = useState([])
+
+  const handleSetImagesArray = (images) => {
+    setImagesArray(images)
+  }
+
+  const childRef = useRef()
+
+  return (
+    <Fragment>
+      <ImageUploader
+        ref={childRef}
+        width='450px'
+        height='450px'
+        imagesArray={imagesArray}
+        handleSetImagesArray={handleSetImagesArray}
+        isDragNotAcceptColor='rgba(0, 0, 0, 0.3)'
+        isDragAcceptColor='#18840f'
+        isDragRejectColor='#ff0000'
+        multipleFiles={true}
+        apiEndpoint='http://localhost:5000/admin/products/publish/media'
+      />
+      <button
+        onClick={() => {
+          childRef.current.handleStartUploadingFiles()
+        }}
+      >
+        Send
+      </button>
+    </Fragment>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,114 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, {
+  useEffect,
+  useState,
+  forwardRef,
+  useImperativeHandle
+} from 'react'
+import Uploader from './components/Uploader'
+import FilesList from './components/FilesList'
+import axios from 'axios'
+// import PropTypes from 'prop-types'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+export const ImageUploader = forwardRef((props, ref) => {
+  const {
+    width,
+    height,
+    imagesArray,
+    handleSetImagesArray,
+    isDragNotAcceptColor,
+    isDragAcceptColor,
+    isDragRejectColor,
+    multipleFiles,
+    apiEndpoint
+  } = props
+  const [processedFilesArray, setProcessedFilesArray] = useState([])
+
+  useEffect(() => {
+    console.log(imagesArray)
+  }, [imagesArray])
+
+  useEffect(
+    () => () => {
+      imagesArray.map((image) => {
+        URL.revokeObjectURL(image.preview)
+      })
+    },
+    []
+  )
+
+  useEffect(() => {
+    handleSetImagesArray(processedFilesArray)
+  }, [processedFilesArray])
+
+  const handleDeleteImage = async (index) => {
+    const tempImagesArray = [...imagesArray]
+    tempImagesArray.splice(index, 1)
+    handleSetImagesArray(tempImagesArray)
+  }
+
+  const handleProcessFiles = () => {
+    imagesArray.map(async (file) => {
+      const formData = new FormData()
+      formData.append('file', file.file)
+      await handleUploadFile(formData)
+    })
+    // handleSetImagesArray(processedFilesArray);
+  }
+
+  const handleUploadFile = async (formData, processedFilesArray) => {
+    axios
+      .post(`${apiEndpoint}`, formData)
+      .then(async (response) => {
+        console.log('response:', response)
+        setProcessedFilesArray((oldProcessedFilesArray) => [
+          ...oldProcessedFilesArray,
+          response
+        ])
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  useImperativeHandle(ref, () => ({
+    handleStartUploadingFiles() {
+      handleProcessFiles()
+    }
+  }))
+
+  return (
+    <div>
+      <Uploader
+        width={width}
+        height={height}
+        handleSetImagesArray={handleSetImagesArray}
+        isDragNotAcceptColor={isDragNotAcceptColor}
+        isDragAcceptColor={isDragAcceptColor}
+        isDragRejectColor={isDragRejectColor}
+        multipleFiles={multipleFiles}
+      >
+        {imagesArray.length > 0 && (
+          <FilesList
+            imagesArray={imagesArray}
+            handleDeleteImage={handleDeleteImage}
+            width={width}
+            height={height}
+          />
+        )}
+      </Uploader>
+    </div>
+  )
+})
+
+// ImageUploader.PropTypes = {
+//   ref: PropTypes.shape().isRequired,
+//   width: PropTypes.string.isRequired,
+//   height: PropTypes.string.isRequired,
+//   imagesArray: PropTypes.array.isRequired,
+//   handleSetImagesArray: PropTypes.func.isRequired,
+//   isDragNotAcceptColor: PropTypes.string.isRequired,
+//   isDragAcceptColor: PropTypes.string.isRequired,
+//   isDragRejectColor: PropTypes.string.isRequired,
+//   multipleFiles: PropTypes.bool.isRequired,
+//   apiEndpoint: PropTypes.string.isRequired
+// }
